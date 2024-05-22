f1816643d109fc9bb20293127a2c1f91b8470ceb
Tadeu Souza <tadeu.souza@codeitsolutions.com.br>
2024-05-22 14:50:50 -0300

    Mock

---
+++ Juntos.Api.Users.Repository/Maps/UserMap.cs
@@ -0,0 +1,22 @@
+﻿using Juntos.Api.Users.Domain.DTOs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juntos.Api.Users.Infrastructure.Maps
+{
+    public class UserMap : IEntityTypeConfiguration<UserDTO>
+    {
+        public void Configure(EntityTypeBuilder<UserDTO> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Login).IsRequired();
+            builder.Property(x => x.Password);
+            builder.Property(x => x.Email);                
+        }
+    }
+}
